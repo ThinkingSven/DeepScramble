486133c969fbebbdf468e4b1df6947484f4b0e9a
SvennyBOI <psven595@gmail.com>
2022-06-10 19:52:54 +0200

    CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import './App.css';
-import Scrambler from "./components/Scrambler"
-import Timer from "./components/Timer"
+import { useState, useEffect } from "react";
+import "./App.css";
+import Scrambler from "./components/Scrambler";
+import Timer from "./components/Timer";
+
 function App() {
+  useEffect(() => {
+    document.addEventListener("keydown", handleSpace);
+  }, []);
+
+  const [toggle, setToggle] = useState(false);
+  function handleSpace(e) {
+    if (e.keyCode == 32) {
+      setToggle(!toggle)
+      console.log(toggle.toString())
+    }
+  }
   return (
     <div className="App">
-      <Timer/>
+      <Timer />
+      <h2>Shortcuts:</h2>
+      <p>Press <a>space</a> to start/stop the timer.</p>
+      <p>Press <a>enter</a> to get to the next solve.</p>
     </div>
   );
 }
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,6 +3,14 @@ import React, {useEffect, useState} from "react";
 function Timer() {
   const [thetimer, setTimer] = useState(0);
   const [timerOn, setTimerOn] = useState(false);
+  const [showStart, setShowStart] = useState(true);
+  const [scramble, setScramble] = useState("L' R2 F2 L' F2 D2 R B2 F2 D2 F D' L2 F2 D R F' L'")
+  function handleReset() {
+    setTimer(0)
+    setTimerOn(false)
+    setScramble("U' R F B' R' U2 R U D F R2 F2 U2 B L2 B R2 B' R2 B D2")
+    setShowStart(true);
+  }
 
   useEffect(() => {
     let interval = null;
@@ -11,6 +19,7 @@ function Timer() {
       interval = setInterval(() => {
         setTimer(prevTime => prevTime + 10)
       }, 10)
+      setShowStart(false);
     }else{
       clearInterval(interval)
     }
@@ -20,11 +29,12 @@ function Timer() {
   }, [timerOn])
   return (
     <div>
-      <p>Time: {timerOn.toString()}</p>
-      <p>Time: {thetimer}</p>
-      <button onClick={() => setTimerOn(true)}>Start</button>
+      <h1>{Math.round(thetimer/1000)}s</h1>
+      <p>{scramble}</p>
+      {showStart ?  <button onClick={() => setTimerOn(true)}>Start</button> : <></> }
       <button onClick={() => setTimerOn(false)}>Stop</button>
-      <button onClick={() => setTimer(0)}>Reset</button>
+      {timerOn ? <></> : <button onClick={handleReset}>Next Solve</button>}
+
     </div>
   );
 }
